7041c8cb7ca24fd595e6fc55a30bf0c432ba5392
Snežana Lalatović <snezanalalatovic@gmail.com>
2020-01-05 07:58:13 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,7 @@
 export const environment = {
   production: false,
   firebaseConfig:{
-    apiKey: "AIzaSyCizh6Ed2a5MyzYvS3b3SuAja9U7QOsSu0",
-    authDomain: "rest-countries-8b733.firebaseapp.com",
-    databaseURL: "https://rest-countries-8b733.firebaseio.com",
-    projectId: "rest-countries-8b733",
-    storageBucket: "rest-countries-8b733.appspot.com",
-    messagingSenderId: "568980219533",
-    appId: "1:568980219533:web:2279a332469741224864c4"
+
     }
 };
 
